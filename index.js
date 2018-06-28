daeca9bbeec22b70552a516c5123eb21b73880ad
Emarr1113 <40649991+Emarr1113@users.noreply.github.com>
2018-06-27 20:15:07 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// controls for changing the background
+document.getElementById('background-btn').addEventListener('click', function() {
+    document.getElementById('background').style.backgroundImage = "url('../assets/R6Background.jpeg')";
+});
+
+
+
+// button controls
+
+// controls for the left button
+document.getElementById('left').addEventListener('click', function() {
+
+   var position = document.getElementById('ball').style.left;
+
+   var newPosition= parseInt(position) - 10;
+
+   document.getElementById('ball').style.right= newPosition + 'px';
+});
+
+// controls for the right button
+document.getElementById('right').addEventListener('click', function() {
+
+    var position = document.getElementById('ball').style.left;
+
+    var newPosition = parseInt(position) + 10;
+
+    document.getElementById('ball').style.left = newPosition + 'px';
+});
+
